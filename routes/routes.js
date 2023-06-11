dc641eb96f3a2a8feb88a6906025ab6de81e4c5c
koech-kiptoo <hoseakiptoo15@gmail.com>
2023-06-11 23:59:04 +0300

    First Commit

--- routes/routes.js
+++ routes/routes.js
@@ -27,5 +27,65 @@ router.post("/add", async (req, res)=>{
 });
 
 
+//GET
+router.get("/employees", async (req, res)=> {
+       try {
+        var result = await Employee.find({})
+        res.status(200).json({ 'message': result })
+        }
+        catch (err) {
+            res.status(400).json({ 'message': err.message })
+        }
+})
+
+
+
+//GET by Name
+router.get("/employees/byname", async (req, res) => {
+    try {
+        var first_name = req.body.first_name
+        var result = await Employee.find({ 'first_name': first_name })
+        res.status(200).json({ 'message': result })
+    }
+    catch (err) {
+        res.status(400).json({ 'message': err.message })
+    }
+});
+
+
+//PUT
+router.put("/employees/update", async (req, res) => {
+    try {
+        var first_name = req.body.first_name
+        var qualification = req.body.qualification
+
+        var result = await Employee.updateOne({ 'first_name': first_name }, {
+            $set: {
+                qualification: qualification
+            }
+        });
+
+        res.status(200).json({ 'message': result })
+    }
+    catch (err) {
+        res.status(400).json({ 'message': err.message })
+    }
+});
+
+//DELETE
+router.delete("/employees/delete", async (req, res) => {
+    try {
+        var first_name = req.body.first_name
+
+        var result = await Employee.deleteOne({ 'first_name': first_name });
+
+        res.status(200).json({ 'message': result })
+    }
+    catch (err) {
+        res.status(400).json({ 'message': err.message })
+    }
+});
+
+
 
 module.exports = router;
